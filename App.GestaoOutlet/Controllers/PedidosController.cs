c3c2c0536177556ba9aac9e84f4d1fb74d1ab01f
celobittar <celobittar@gmail.com>
2022-12-28 21:51:41 -0300

    Atualização - Versão nao funcional

--- App.GestaoOutlet/Controllers/PedidosController.cs
+++ App.GestaoOutlet/Controllers/PedidosController.cs
@@ -1,13 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using App.GestaoOutlet.Data;
+using App.GestaoOutlet.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace App.GestaoOutlet.Controllers
 {
     public class PedidosController : Controller
     {
-        public IActionResult Pedidos()
+        private readonly DboutletContext _context;
+
+        public PedidosController(DboutletContext context)
         {
-            return View();
+            _context = context;
         }
+
+        public async Task<IActionResult> Index(int IDPedido, [Bind("IDPedido,IDFormaPag,EnderecoEntrega,DtEntrega,VlrGasto,VlrRecebido,Qtde,IDStatus,IDPedidoCategoria,IDPedidoModelo,FaltaPagar")] Pedido pedido)
+        {
+            IQueryable<int> pedidoQuery = from m in _context.Pedidos
+                                          orderby m.Idpedido
+                                          select m.Idpedido;
+
+            var pedidos = from m in _context.Pedidos select m;
+
+            var PedidosVM = new PedidoViewModel
+            {
+                IdPedidos = new SelectList(await pedidoQuery.Distinct().ToListAsync()),
+                Pedidos = await pedidos.ToListAsync()
+            };
+
+            return View(PedidosVM);
+        }
+
 
         public IActionResult Create()
         {
---
+++ App.GestaoOutlet/Models/PedidoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace App.GestaoOutlet.Models
+{
+    public class PedidoViewModel
+    {
+        public List<Pedido> Pedidos { get; set; }
+        public SelectList IdPedidos { get; set; }
+        public int IdPedido { get; set; }
+        public string SearchIDPedido { get; set; }
+    }
+}
